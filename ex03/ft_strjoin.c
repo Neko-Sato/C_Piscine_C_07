80417b810871285e7eee03b02b388e405bd0fb10
hshimizu <hisatosi35350@gmail.com>
2023-03-20 13:21:31 +0900

    ヌルが保証されていなかったので、よくわからんメモリについてセグフォでした！

--- ex03/ft_strjoin.c
+++ ex03/ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: hshimizu <hshimizu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/19 15:15:45 by hshimizu          #+#    #+#             */
-/*   Updated: 2023/03/19 19:15:36 by hshimizu         ###   ########.fr       */
+/*   Updated: 2023/03/20 13:20:42 by hshimizu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,13 +30,13 @@ char	*ft_strjoin(int size, char **strs, char *sep)
 
 	len = 1 + ft_strlen(sep) * (size - 1);
 	i[0] = 0;
-	while (strs[i[0]])
+	while (i[0] < size)
 		len += ft_strlen(strs[i[0]++]);
 	result = (char *)malloc((len + 1) * sizeof(char));
 	i[0] = 0;
 	i[1] = 0;
 	i[2] = 0;
-	while (strs[i[1]])
+	while (i[1] < size)
 	{
 		while (strs[i[1]][i[2]])
 			result[i[0]++] = strs[i[1]][i[2]++];
