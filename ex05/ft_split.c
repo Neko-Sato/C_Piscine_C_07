6a626cdf7f3b3ae95f33b536e892de66d8cb7a92
hshimizu <hisatosi35350@gmail.com>
2023-03-20 14:09:26 +0900

    ヌルにすることにした

--- ex05/ft_split.c
+++ ex05/ft_split.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   split.c                                            :+:      :+:    :+:   */
+/*   ft_split.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: hshimizu <hshimizu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/20 09:29:27 by hshimizu          #+#    #+#             */
-/*   Updated: 2023/03/20 10:43:47 by hshimizu         ###   ########.fr       */
+/*   Updated: 2023/03/20 13:31:12 by hshimizu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -89,7 +89,7 @@ char	**ft_split(char *str, char *charset)
 
 	word_count = get_word_count(str, charset);
 	charset_len = ft_strlen(charset);
-	result = (char **)malloc((word_count + 2) * sizeof(char *));
+	result = (char **)malloc((word_count + 1) * sizeof(char *));
 	i = 0;
 	while (i < word_count)
 	{
@@ -97,9 +97,24 @@ char	**ft_split(char *str, char *charset)
 		str += ft_strlen(result[i]) + charset_len;
 		i++;
 	}
-	result[i] = (char *)malloc(2 * sizeof(char));
-	ft_strlcpy(result[i], "0", 2);
-	i++;
-	result[i] = NULL;
+	result[i] = 0;
 	return (result);
+}
+
+#include <stdio.h>
+
+int	main(void)
+{
+	char *str;
+	char *charset;
+	char **ans;
+
+	str = "_hello__";
+	charset = "+_";
+	ans = ft_split(str, charset);
+	printf("0: %s\n", ans[0]);
+	printf("1: %s\n", ans[1]);
+	printf("2: %s\n", ans[2]);
+	printf("3: %s\n", ans[3]);
+	printf("4: %s\n", ans[4]);
 }
